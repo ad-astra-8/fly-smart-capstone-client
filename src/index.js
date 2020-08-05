75be38c5ab4406311b4bb0a08dcc308a760b2053
Wanda Antoine <wanda@thinkful.com>
2020-08-04 23:30:52 -0400

    commented out footer for now

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import 'font-awesome/css/font-awesome.min.css';
 import './index.css';
 import App from './App';
-import Footer from './Footer';
+// import Footer from './Footer';
 
 
 
 ReactDOM.render(
   <>
     <App />
-    <Footer />
+    {/* <Footer /> */}
   </>,
   document.getElementById('root')
 );
