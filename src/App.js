e31efbc2510e17d2847d907e3c328c51c30d03a1
Wanda Antoine <wanda@thinkful.com>
2020-08-21 12:01:27 -0400

    cleaned code

--- src/App.js
+++ src/App.js
@@ -5,47 +5,24 @@ import LandingPage from "./LandingPage";
 import HomePage from "./HomePage";
 import MyNoteForm from "./MyNoteForm";
 import Checklist from "./Checklist";
-// import Login from './Login';
 import Register from './Register';
 // import Data from './Data';
-// import MyNotes from './MyNotes';
 
 
 class App extends Component {
   state = {
     // notes: ['note 1', 'note 2', 'note 3'],
     notes: [],
-    // checklist: [],
-    // completed: false
-
   };
 
   componentDidMount() {
-    // Promise.all([
-    //   fetch(`${config.API_ENDPOINT}/notes`)
-    // ])
-    //   .then(([notesRes]) => {
-    //     if (!notesRes.ok)
-    //       return notesRes.json().then(e => Promise.reject(e));
 
-    //     return Promise.all([notesRes.json()]);
-    //   })
-    //   .then(([notes]) => {
-    //     this.setState({ notes });
-    //   })
-    //   .catch(error => {
-    //     console.error({ error });
-    //   });
-    // }
-
-    // componentDidMount() {
     let getCollectionByUserId = `${config.API_ENDPOINT}/notes`;
     //  ${TokenService.getUserId()};
 
     fetch(getCollectionByUserId)
       .then((response) => response.json())
       .then((data) => {
-        // console.log(data)
         this.setState({
           notes: data,
         });
@@ -81,18 +58,11 @@ class App extends Component {
               <Route exact path="/homepage" component={HomePage} />
               <Route path="/register" component={Register} />
               <Route path="/checklist" component={Checklist} />
-              {/* <Route path="/my-list" component={Mylist} /> */}
               <Route
                 exact
                 path="/my-notes"
                 render={(props) => <MyNoteForm numTodos={this.state.notes.length} notes={this.state.notes} onDeleteNote={this.handleDeleteNote} onAddNote={this.handleAddNote} />}
               />
-
-              {/* <Route
-                exact
-                path="/my-list"
-                render={(props) => <MyList numTodos={this.state.notes.length} notes={this.state.notes} onDelete={this.handleDelete} onFormSubmit={this.handleAddNote} />}
-              /> */}
             </Switch>
           </BrowserRouter>
 
